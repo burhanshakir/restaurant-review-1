843eee48367e430826f43c1fdee4885204cca7ed
bshakir <burhanshakir96@gmail.com>
2018-05-28 17:13:51 +0100

    Storing cache

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,49 @@
-self.addEventListener('fetch', function(event){
-  console.log(event.request)
-})
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('restaurant-static-v1')
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+// self.addEventListener('activate', function(event) {
+//   console.log('Finally active. Ready to start serving content!');
+// });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
